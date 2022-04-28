4b14f9c04120defaead261a38f5bfb7d487d6a33
K0966014 <nattawat.k@kbtg.tech>
2022-04-28 15:10:15 +0700

    New workshop

---
+++ src/workshop/Workshop.jsx
@@ -0,0 +1,16 @@
+import Hello from "./Hello"
+
+const Workshop = () => {
+    const callback = (message) => {
+        console.log("Callback with " + message);
+    }
+
+    return (
+        <div>
+            <h2>Workshop component</h2>
+            <Hello initialName="Default name" onCall={callback} />
+        </div>
+    )
+}
+
+export default Workshop
