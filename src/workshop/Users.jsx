c95cb44cfcb63d11620f1b79275ea675a603e031
K0966014 <nattawat.k@kbtg.tech>
2022-04-28 16:36:27 +0700

    useEffect display user

--- src/workshop/Users.jsx
+++ src/workshop/Users.jsx
@@ -2,23 +2,27 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 const Users = () => {
-    const [users, setUsers] = useState([])
+    const [user, setUser] = useState({})
+    const [counter, setCounter] = useState(1)
 
     // Call data from api https://jsonplaceholder.typicode.com/users
     useEffect(() => {
         console.log("Use effect");
-        axios.get("https://jsonplaceholder.typicode.com/users")
+        axios.get("https://jsonplaceholder.typicode.com/users/" + counter)
             .then(function (response) {
-                console.log(response)
-                setUsers(response.data)
+                // console.log(response)
+                setUser(response.data)
             })
-    }, [])
+    }, [counter])
 
-    console.log(users);
+    console.log(user);
+
     return (
         <div>
             Demo with API.
-            {users.map(user => <li key={user.id}>{user.name}</li>)}
+            <button onClick={() => setCounter(counter + 1)}>Add</button>
+            {/* {users.map(user => <li key={user.id}>{user.name}</li>)} */}
+            <h2>{user.name}</h2>
         </div>
     )
 }
